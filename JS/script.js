c80cff117f488f20637706b689d2f020b860f6dc
JanKosicar <132477714+JanKosicar@users.noreply.github.com>
2023-11-22 17:47:44 +0100

    main page complete

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = document.getElementById('sliderContainer');
+    let isMoving = false;
+
+    slider.addEventListener('mouseenter', () => {
+        isMoving = true;
+    });
+
+    slider.addEventListener('mouseleave', () => {
+        isMoving = false;
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+        if (isMoving) {
+            const containerWidth = document.getElementById('photoSlider').offsetWidth;
+            const sliderWidth = slider.offsetWidth;
+
+            const moveRatio = (e.clientX / containerWidth) * (sliderWidth - containerWidth);
+            slider.style.transform = `translateX(-${moveRatio}px)`;
+        }
+    });
+});
